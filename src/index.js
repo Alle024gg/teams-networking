dc426cee131f19615731f13848e63277e9e48a86
Alle024gg <alle024ggnanana@gmail.com>
2024-03-11 21:02:33 +0200

    startEdit with id

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import "./style.css";
 
+let allTeams = [];
+
 function $(selector) {
   return document.querySelector(selector);
 }
@@ -64,6 +66,7 @@ function loadTeams() {
   const promise = fetch("http://localhost:3000/teams-json")
     .then(r => r.json())
     .then(teams => {
+      allTeams = teams;
       renderTeams(teams);
       return teams;
     });
@@ -86,6 +89,13 @@ function onSubmit(e) {
   window.location.reload();
 }
 
+function startEdit(id) {
+  const team = allTeams.find(team => {
+    return id === team.id;
+  });
+  console.warn("click on edit %o", id, team);
+}
+
 function initEvents() {
   $("#teamsForm").addEventListener("submit", onSubmit);
   $("#teamsTable tbody").addEventListener("click", e => {
@@ -94,7 +104,9 @@ function initEvents() {
       deleteTeamRequest(id);
       window.location.reload();
     } else if (e.target.matches("a.edit-btn")) {
-      console.warn("click on edit");
+      // const id = e.target.getAttribute("data-id");
+      const id = e.target.dataset.id;
+      startEdit(id);
     }
   });
 }
